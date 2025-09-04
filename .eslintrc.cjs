1419c33cb0c9d48ca7b42a78405519fd0c215c1f
Tobias <tobiasorban00@gmail.com>
2025-09-04 19:38:15 -0300

    chore(lint): ignorar dist y configurar entornos node/browser + react-hooks

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,35 @@
-/** @type {import('eslint').Linter.Config} */
+// .eslintrc.cjs
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'react-hooks'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended'
   ],
-  ignorePatterns: ['dist/**'],
+  ignorePatterns: [
+    'dist',
+    'packages/*/dist',
+    'build',
+    'coverage',
+    '.vite'
+  ],
+  overrides: [
+    // Archivos CLI (Node.js)
+    {
+      files: ['packages/cli/**/*.{ts,tsx,js}'],
+      env: { node: true }
+    },
+    // Archivos Web/React (Browser)
+    {
+      files: ['packages/web/**/*.{ts,tsx,js}', 'src/**/*.{ts,tsx,js}'],
+      env: { browser: true },
+      rules: {
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn'
+      }
+    }
+  ],
   rules: {
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-explicit-any': 'off'
